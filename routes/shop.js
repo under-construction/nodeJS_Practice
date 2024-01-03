d4c9e4926abde3b1718f6ad6a772a53ba1273ad6
Ercüment Kalkan <ercukalkann@gmail.com>
2024-01-03 13:25:03 +0300

    serving html pages with response
    error with the file path

--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('In middleware for express.js page (shop.js)');
-    res.send('<h1>Hello from Express.js</h1>');
+    res.sendFile('./views/shop.html');
 });
 
 
