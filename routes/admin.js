b2c70e30b73204018c6bc24e255d15e66dbb48cc
Ercüment Kalkan <ercukalkann@gmail.com>
2024-01-04 16:51:34 +0300

    using path package to filter paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 router.get('/add-product', (req, res, next) => {
     console.log('In middleware for add product (admin.js)');
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product1234', (req, res, next) => {
