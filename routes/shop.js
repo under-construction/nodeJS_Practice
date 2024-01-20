f3eeeaaa4feacc764817b65a960671170356128f
Ercüment Kalkan <ercukalkann@gmail.com>
2024-01-21 00:07:01 +0300

    finishing Pug

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-    res.render('shop123', { prods: adminData.products, docTitle123: 'Shop123'});
+    res.render('shop123', { prods: adminData.products, docTitle123: 'Shop123', path123: '/shop'});
 });
 
 
