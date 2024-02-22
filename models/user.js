2a127560d1b4553f456c1ae5b840ff1a48f88743
Ercument <ercukalkann@gmail.con>
2024-02-22 10:20:46 +0300

    user model created

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Product = require('./product');
+
+const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = User;
