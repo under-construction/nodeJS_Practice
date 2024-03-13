6c8516f31f5fad2eeef643fac081935bda9d63a3
Ercument <ercukalkann@gmail.con>
2024-03-13 14:57:55 +0300

    mongo db connection

--- util/database.js
+++ util/database.js
@@ -1,15 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const uri = 'mongodb+srv://sa:123@mongodbpractice123.zxtp6fe.mongodb.net/?retryWrites=true&w=majority&appName=mongoDBPractice123';
 
 const mongoConnect = cb => {
     MongoClient.connect(uri)
         .then(result => {
-            console.log('mongodb successfully connected...')
+            _db = result.db();
             cb(result);
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            console.log(err)
+        });
 }
 
-module.exports = mongoConnect;
+const getDB = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
