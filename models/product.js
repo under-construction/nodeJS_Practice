ca66f7b740a0727d238042bd15540c62cc20fe07
Ercument <ercukalkann@gmail.con>
2024-03-15 19:14:25 +0300

    post edit product page

--- models/product.js
+++ models/product.js
@@ -2,17 +2,31 @@ const { getDB } = require('../util/database');
 const { ObjectId } = require('mongodb');
 
 class Product {
-    constructor(title, price, description, imageUrl) {
+    constructor(title, price, description, imageUrl, id) {
         this.title = title;
         this.price = price;
         this.description = description;
         this.imageUrl = imageUrl;
+        this._id = ObjectId.createFromHexString(id);
     }
 
     save() {
         const db = getDB();
-        return db.collection('products')
-            .insertOne(this)
+        let dbOp;
+        if (this._id) {
+            dbOp = db.collection('products').
+                updateOne(
+                    {
+                        _id: this._id
+                    },
+                    {
+                        $set: this
+                    });
+        }
+        else {
+            dbOp = db.collection('products').insertOne(this);
+        }
+        return dbOp
             .then(result => {
                 console.log(result);
             })
