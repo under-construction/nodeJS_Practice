0fcf3da6ddf7edd7cfee1b55adb5a9c77da96cf5
Ercument <ercukalkann@gmail.con>
2024-03-19 17:54:35 +0300

    add new or existing product to cart (PART 4)

--- models/user.js
+++ models/user.js
@@ -23,13 +23,23 @@ class User {
     }
 
     addToCart(product) {
-        // const cartProduct = this.cart.findIndex(cp => cp._id === product._id);
-        const updatedCart = {
-            items: [{
+        const cartProductIndex = this.cart.items.findIndex(cp => {
+            return cp.productId.toString() === product._id.toString();
+        });
+        let newQuantity = 1;
+        let updatedCartItems = [...this.cart.items];
+
+        if (cartProductIndex >= 0) {
+            newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+            updatedCartItems[cartProductIndex].quantity = newQuantity;
+        } else {
+            updatedCartItems.push({
                 productId: product._id,
-                quantity: 1
-            }]
-        };
+                quantity: newQuantity
+            })
+        }
+        const updatedCart = { items: updatedCartItems };
+
         const db = getDB();
         return db
             .collection('users')
