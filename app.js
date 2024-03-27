0f39fe9d08132d0e83914129e6114582601b9709
Ercument <ercukalkann@gmail.con>
2024-03-27 22:30:33 +0300

    user model and adding default user to the request with mongoose

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 
 const notFound404Controller = require('./controllers/404');
 const { run } = require('./util/database');
+const User = require('./models/user');
 
 const PORT = 3080;
 
@@ -21,21 +22,20 @@ app.use(express.static(path.join(__dirname, 'public1234')));
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// app.use((req, res, next) => {
-// User.getById('65f99bd4f6142d4838219906')
-//     .then(user => {
-//         // ANYTHING CAN BE ATTACHED TO ANY REQUEST VIA MIDDLEWARES FOR FURTHER USE ANYWHERE.
-//         req.user = new User(user.name, user.email, user.cart, user._id);
-//         req.x = 1;
-//         next();
-//     })
-//     .then(() => {
-//         console.log('*****************');
-//         // next();
-//     })
-//     .catch(err => console.log(err));
-// next();
-// });
+app.use((req, res, next) => {
+    User.findById('66046aabdbc6c99b11c7033c')
+        .then(user => {
+            // ANYTHING CAN BE ATTACHED TO ANY REQUEST VIA MIDDLEWARES FOR FURTHER USE ANYWHERE.
+            req.user = user;
+            req.x = 1;
+            next();
+        })
+        .then(() => {
+            console.log('*****************');
+        })
+        .catch(err => console.log(err));
+    // next();
+});
 
 app.use('/admin123', adminRoutes);
 app.use(shopRoutes);
@@ -53,6 +53,17 @@ app.use(notFound404Controller.notFound404);
 
 async function main() {
     await mongoose.connect(uri);
+    const ifAUserExists = await User.findOne();
+    if (!ifAUserExists) {
+        const user = new User({
+            name: 'ercu',
+            email: 'ercu@test.com',
+            cart: {
+                items: []
+            }
+        });
+        await user.save();
+    }
     app.listen(PORT);
 }
 
