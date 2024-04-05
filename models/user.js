864a7de1de009b3d102d0c733ecaccb3e76a4174
Ercument <ercukalkann@gmail.con>
2024-04-05 16:20:18 +0300

    delete product from cart with mongoose

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Product = require('./product');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -69,6 +70,42 @@ userSchema.methods.addToCart = async function (product) {
 
 userSchema.methods.getCart = async function () {
     return this;
+}
+
+userSchema.methods.deleteProductFromCart = async function (productId) {
+    const removedCartItemIndex = this.cart.items.findIndex(i => {
+        return i.productId.toString() === productId.toString();
+    });
+
+    let newQuantity;
+    let newCartTotalPrice;
+    let updatedCartItems = [...this.cart.items];
+
+    const removedProduct = await Product
+        .findById(productId)
+        .select('price -_id');
+
+    newQuantity = updatedCartItems[removedCartItemIndex].quantity - 1;
+    newCartTotalPrice = this.cart.totalPrice - removedProduct.price;
+
+    if (newQuantity === 0) {
+        updatedCartItems = updatedCartItems.filter(i => {
+            return i.productId.toString() !== productId.toString();
+        });
+    }
+
+    const updatedCart = {
+        items: updatedCartItems,
+        totalPrice: newCartTotalPrice
+    };
+
+    this.cart = updatedCart;
+
+    return await this.save();
+}
+
+userSchema.methods.recalculateCartTotalPrice = function (productId) {
+
 }
 
 module.exports = mongoose.model('User', userSchema);
