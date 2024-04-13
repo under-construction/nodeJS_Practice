a8e99cfcb12bc7f91eec92693fc16afee6e57af7
Ercument <ercukalkann@gmail.con>
2024-04-13 23:01:06 +0300

    creating a cookie and using it across the requests

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,19 @@
 exports.getLogin = (req, res, next) => {
+    const cookie = req.get('Cookie');
+    let isLoggedIn = false;
+
+    if (cookie) {
+        isLoggedIn = cookie.split('=')[1];
+    }
+
     res.render('auth456/login456', {
         path123: '/auth444/login444',
-        pageTitle123: 'Login'
+        pageTitle123: 'Login',
+        isAuthenticated: isLoggedIn
     });
 }
 
 exports.postLogin = (req, res, next) => {
-    console.log('login request has been sent');
+    res.setHeader('Set-Cookie', 'loggedIn=true');
     res.redirect('/');
 }
