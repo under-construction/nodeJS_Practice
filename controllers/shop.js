ab79c6179c1f70c71213cbf4c7428c39f27de59a
Ercument <ercukalkann@gmail.con>
2024-05-02 13:09:12 +0300

    downloading a pdf

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,7 @@
 const Product = require('../models/product');
 const Order = require('../models/order');
+const fs = require('fs');
+const path = require('path');
 
 exports.getProducts = async (req, res, next) => {
     try {
@@ -163,4 +165,18 @@ exports.getProductDetail = async (req, res, next) => {
         error.httpStatusCode = 500;
         return next(err);
     }
+}
+
+exports.getInvoice = (req, res, next) => {
+    const orderId = req.params.orderId;
+    const invoiceName = 'invoice-' + orderId + '.pdf';
+    const invoicePath = path.join('data', 'invoices', invoiceName);
+    fs.readFile(invoicePath, (err, data) => {
+        if (err) {
+            return next(err);
+        }
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+        res.send(data);
+    });
 }
